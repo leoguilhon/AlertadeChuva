6378056ad1db991d331a8a4e5950bac4e82165e2
leoguilhon <115726836+leoguilhon@users.noreply.github.com>
2023-01-12 19:06:30 -0300

    0.5
    Uma versão simples, ainda como um alerta de previsão do tempo inicializado manualmente.
    
    Para as próximas versões:
    - Adicionar execução diária do código
    - Estudar a possibilidade de ser um aviso apenas com pancadas de chuva, de forma que avise em tempo real.

--- Alerta de Chuva.py
+++ Alerta de Chuva.py
@@ -1,14 +1,22 @@
 # Alerta de Chuva
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import os
 
 contato = "importante" # Nome do contato para o qual será enviado o aviso via Whatsapp
 
+# Definir as configurações do webdriver para o navegador pessoal
+CHROME_DATA_PATH = "user-data-dir=C:\\Users\\Usuario\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+os.system("taskkill /im chrome.exe /f")
+options = webdriver.ChromeOptions()
+options.add_argument(CHROME_DATA_PATH)
+
 # Coletar a informação
 
-navegador = webdriver.Chrome()
+navegador = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
 
 navegador.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/321/riodejaneiro-rj")
 chuva = navegador.find_element("xpath",'//*[@id="mainContent"]/div[4]/div[5]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').get_attribute("innerText")
@@ -45,5 +53,7 @@ if (tipodechuva != "Não há chuva"):
    navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(f"Alerta de Chuva! Olá, {contato}! Já pode tirar o seu guarda-chuva do armário, hoje irá chover com uma precipitação de {chuvaflt:.0f}mm na cidade do Rio de Janeiro. Status da chuva: {tipodechuva}.")
    navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
    time.sleep(5)
+  
+   
    
 
