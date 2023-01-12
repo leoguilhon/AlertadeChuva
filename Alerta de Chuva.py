12855d6deeeb89ec737584bc77bc6427f8ccab9f
leoguilhon <115726836+leoguilhon@users.noreply.github.com>
2023-01-12 18:00:48 -0300

    0.1
    Uma versão simples, ainda como um alerta de previsão do tempo.
    
    Para as próximas versões:
    - Automatizar a conexão ao Whatsapp
    - Adicionar execução diária do código
    - Estudar  a possibilidade de ser um aviso apenas com pancadas de chuva.

--- Alerta de Chuva.py
+++ Alerta de Chuva.py
@@ -3,10 +3,46 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# Alerta de Chuva
+contato = "importante" # Nome do contato para o qual será enviado o aviso via Whatsapp
+
+# Coletar a informação
 
 navegador = webdriver.Chrome()
 
 navegador.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/321/riodejaneiro-rj")
-chuva = navegador.find_element("xpath",'//*[@id="mainContent"]/div[4]/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').get_attribute("data-value")
+chuva = navegador.find_element("xpath",'//*[@id="mainContent"]/div[4]/div[5]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').get_attribute("innerText")
 print(chuva)
+
+# Tratar o dado
+
+chuvaflt = float(chuva[0:2])
+print(chuvaflt)
+
+# Definir a intensidade da chuva
+tipodechuva = str
+if (chuvaflt == 0):
+  tipodechuva = "Não há chuva"
+if (chuvaflt < 5):
+  tipodechuva = "Chuva fraca"
+  print(tipodechuva)
+if (chuvaflt >= 5 and chuvaflt <=25):
+  tipodechuva = "Chuva moderada"
+if (chuvaflt > 25 and chuvaflt <= 50):
+  tipodechuva = "Chuva forte"
+if (chuvaflt > 50):
+  tipodechuva = "Chuva muito forte"
+print(tipodechuva)
+
+# Enviar alerta de chuva
+if (tipodechuva != "Não há chuva"):
+   navegador.get("https://web.whatsapp.com/")
+   time.sleep(10)
+   while len(navegador.find_elements(By.ID, 'side')) < 1: # Esperar o whatsapp conectar com o QRCode
+    time.sleep(1)
+   navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(contato)
+   navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(Keys.ENTER)
+   navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(f"Alerta de Chuva! Olá, {contato}! Já pode tirar o seu guarda-chuva do armário, hoje irá chover com uma precipitação de {chuvaflt:.0f}mm. Status da chuva: {tipodechuva}.")
+   navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
+   time.sleep(5)
+   
+
