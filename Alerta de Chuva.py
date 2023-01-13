2fcb9ca979313edb6ef0cb4c954b67f43c64947a
leoguilhon <115726836+leoguilhon@users.noreply.github.com>
2023-01-13 11:43:22 -0300

    Update Alerta de Chuva.py

--- Alerta de Chuva.py
+++ Alerta de Chuva.py
@@ -1,6 +1,7 @@
 # Alerta de Chuva
 
 import time
+import schedule
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -15,45 +16,48 @@ options = webdriver.ChromeOptions()
 options.add_argument(CHROME_DATA_PATH)
 
 # Coletar a informação
+def AlertadeChuva():
+  navegador = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
 
-navegador = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
-
-navegador.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/321/riodejaneiro-rj")
-chuva = navegador.find_element("xpath",'//*[@id="mainContent"]/div[4]/div[5]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').get_attribute("innerText")
-print(chuva)
+  navegador.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/321/riodejaneiro-rj")
+  chuva = navegador.find_element("xpath",'//*[@id="mainContent"]/div[4]/div[5]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').get_attribute("innerText")
+  print(chuva)
 
 
 # Tratar o dado
 
-chuvaflt = float(chuva.split("mm")[0])
-print(chuvaflt)
+  chuvaflt = float(chuva.split("mm")[0])
+  print(chuvaflt)
 
 # Definir a intensidade da chuva
-tipodechuva = str
-if (chuvaflt == 0):
-  tipodechuva = "Não há chuva"
-if (chuvaflt < 5):
-  tipodechuva = "Chuva fraca"
+  tipodechuva = str
+  if (chuvaflt == 0):
+    tipodechuva = "Não há chuva"
+  if (chuvaflt < 5):
+    tipodechuva = "Chuva fraca"
+  if (chuvaflt >= 5 and chuvaflt <=25):
+    tipodechuva = "Chuva moderada"
+  if (chuvaflt > 25 and chuvaflt <= 50):
+    tipodechuva = "Chuva forte"
+  if (chuvaflt > 50):
+    tipodechuva = "Chuva muito forte"
   print(tipodechuva)
-if (chuvaflt >= 5 and chuvaflt <=25):
-  tipodechuva = "Chuva moderada"
-if (chuvaflt > 25 and chuvaflt <= 50):
-  tipodechuva = "Chuva forte"
-if (chuvaflt > 50):
-  tipodechuva = "Chuva muito forte"
-print(tipodechuva)
 
 # Enviar alerta de chuva
-if (tipodechuva != "Não há chuva"):
-   navegador.get("https://web.whatsapp.com/")
-   time.sleep(10)
-   while len(navegador.find_elements(By.ID, 'side')) < 1: # Esperar o whatsapp conectar com o QRCode
-    time.sleep(1)
-   navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(contato)
-   navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(Keys.ENTER)
-   navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(f"Alerta de Chuva! Olá, {contato}! Já pode tirar o seu guarda-chuva do armário, hoje irá chover com uma precipitação de {chuvaflt:.0f}mm na cidade do Rio de Janeiro. Status da chuva: {tipodechuva}.")
-   navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
-   time.sleep(5)
-   
+  if (tipodechuva != "Não há chuva"):
+    navegador.get("https://web.whatsapp.com/")
+    time.sleep(10)
+    while len(navegador.find_elements(By.ID, 'side')) < 1: # Esperar o whatsapp conectar com o QRCode
+      time.sleep(1)
+    navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(contato)
+    navegador.find_element("xpath",'//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]').send_keys(Keys.ENTER)
+    navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(f"Alerta de Chuva! Olá, {contato}! Já pode tirar o seu guarda-chuva do armário, hoje irá chover com uma precipitação de {chuvaflt:.0f}mm na cidade do Rio de Janeiro. Status da chuva: {tipodechuva}.")
+    navegador.find_element("xpath",'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p').send_keys(Keys.ENTER)
+    time.sleep(5)
+    
+schedule.every(1).days.at('09:00').do(AlertadeChuva)
+while 1:
+  schedule.run_pending()
+  time.sleep(1)
    
 
